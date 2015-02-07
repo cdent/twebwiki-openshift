3a9439eb7a7094b2bd75cfeaecc332d4b95a17f2
Chris Dent <chris.dent@gmail.com>
2015-02-07 11:16:26 +0000

    adding deploy hook to make instance

--- tiddlywebconfig.py
+++ tiddlywebconfig.py
@@ -6,7 +6,8 @@ config = {
     'system_plugins': ['tiddlywebwiki'],
     'twanager_plugins': ['tiddlywebwiki'],
     'server_store': ['text', {
-        'store_root': os.environ.get('OPENSHIFT_DATA_DIR')}],
+        'store_root': os.path.join(
+            os.environ.get('OPENSHIFT_DATA_DIR'), 'store'}],
     'server_host': {
         'scheme': 'http',
         'host': os.environ.get('OPENSHIFT_APP_DNS'),
