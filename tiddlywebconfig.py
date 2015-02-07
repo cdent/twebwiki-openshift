84963aeb8430aca5d68da97dc2f4257d2677750b
Chris Dent <chris.dent@gmail.com>
2015-02-07 10:48:44 +0000

    update for basic mod wsgi run

---
+++ tiddlywebconfig.py
@@ -0,0 +1,15 @@
+import os
+
+config = {
+    'secret': os.environ.get('OPENSHIFT_SECRET_TOKEN'),
+    'log_level': 'DEBUG',
+    'system_plugins': ['tiddlywebwiki'],
+    'twanager_plugins': ['tiddlywebwiki'],
+    'server_store': ['text', {
+        'store_root': os.environ.get('OPENSHIFT_DATA_DIR')}],
+    'server_host': {
+        'scheme': 'http',
+        'host': os.environ.get('OPENSHIFT_APP_DNS'),
+        'port': os.environ.get('OPENSHIFT_PYTHON_PORT'),
+    },
+}
